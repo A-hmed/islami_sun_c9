dfebb7daffc568705b20f7b4c95f56dd30cf6cce
omnialasheen <133675765+omnialasheen@users.noreply.github.com>
2023-10-03 19:58:35 +0300

    finished sebha tab

--- lib/ui/utils/app_assets.dart
+++ lib/ui/utils/app_assets.dart
@@ -8,4 +8,8 @@ abstract class AppAssets{
   static const ahadehtTabLogo = "${baseImagesPath}ahadeth_tab_logo.png";
   static const backgorund = "${baseImagesPath}background.png";
   static const darkBackgorund = "${baseImagesPath}backgorund_dark.png";
+  static const sebhaBody = "assets/images/body_sebha_logo.png";
+  static const sebhaBodyDark = "assets/images/body_sebha_dark.png";
+  static const headSebha = "assets/images/head_sebha_logo.png";
+  static const headSebhaDark = "assets/images/head_sebha_dark.png";
 }
